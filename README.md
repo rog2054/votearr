a5111f7ae033d2144982a19c8f8a996ab80a5119
Roger Ransome <52100206+rog2054@users.noreply.github.com>
2024-11-17 17:10:02 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,172 @@
+# votearr
+
+## ❓ Purpose
+
+**tldr : Vote to delete old media from your Radarr library**
+
+This utility gives a simple user-friendly way for multiple viewers of a Radarr library (eg a household) to each cast a vote about if the media should be kept for re-watching, or deleted forever. They also have the option to skip a vote for example if they haven't yet watched it or are currently undecided.
+
+It is provided as a ready-to-build docker image 🐳 and can be run on either Windows or Linux.
+
+Once installed your group use it via a simple web interface on a Computer or Mobile device.
+
+***
+
+## 🚀 Quick Install
+
+### Start in 3 steps ###
+1. Clone this repo
+2. Edit [settings.ini](#configuration)
+3. Build and run the docker image.
+
+*See below for [detailed install instructions]() for Windows or Linux*
+
+***
+<a id="configuration"></a>
+## 📜 Configuration
+
+### settings.ini parameters
+👀 This file contains configuration specific to your installation and must be edited before first use 👀
+
+There are 4 settings to configure. The settings.ini file provided contains examples and comments.
+
+##### radarr_url
+The local ip and port for your existing Radarr installation.
+
+example: `radarr_url=http://192.168.1.24:7878`
+ 
+##### radarr_apikey
+The API key for your existing Radarr.
+
+You can find this within Radarr under the Settings - General tab
+
+example: `radarr_apikey=6c4d96beda244f755427b34f632b4227`
+
+##### voting_users
+
+Users who will be able to vote. The order isn't important, however it must be a comma-separated list of names (with or without a space after the comma).
+
+example: `voting_users = Bob,Alice,Carlos`
+
+valid alternative: `voting_users = Bob, Alice, Carlos`
+
+##### delete_votes_required
+The minimum number of unique users voting 'Delete' for an item before it will be considered for permanent deletion from the Radarr library. It is suggested to set this to the number of users (ie 3 for a 3-person household) so items can only be deleted once nobody wants them anymore.
+
+example: `delete_votes_required=3`
+
+***
+
+## 💡 Detailed Install Instructions
+
+### Setup in Windows
+
+1. Clone this repo into a folder, eg c:\votearr
+2. Edit the settings.ini file - see [Configuration - settings.ini](#configuration) for guidance
+3. If you do not have Docker already download, install, and run [Docker Desktop](https://www.docker.com/products/docker-desktop/) for Windows
+4. Identify your machine IP address `ipconfig`
+5. From the command prompt, build and run the docker image
+
+     c:\votearr> `docker-compose up -build `
+6. Once you are happy it is working, exit this instance with <kbd>Ctrl</kbd>+<kbd>c</kbd> then relaunch as a background container `docker-compose up -d`
+
+
+### Setup in Linux
+(based on a fresh minimal server install of Ubuntu, but any similar distro should be a similar process)
+
+1. Clone this repo into a folder, eg /home/user/votearr
+2. Edit the settings.ini file - see [Configuration - settings.ini](#configuration) for guidance
+3. Install Docker Compose
+`sudo apt-get install docker-compose`
+ 
+   Add the current linux user to the docker group `sudo usermod -aG docker ${USER}`
+
+   Logoff then logon again for the new group membership to apply
+4. Identify your machine IP address `ip a`
+5. Build and run the docker image `docker-compose up --build`
+6. Once you are happy it is working, exit this instance with <kbd>Ctrl</kbd>+<kbd>c</kbd> then relaunch as a background container `docker-compose up -d`
+
+***
+
+## 👪 User Guide
+
+### The votearr web interface
+
+`http://192.168.1.24` 
+
+From a web browser go to http://ipaddress from step 4 above, eg 
+
+*For simplicity the ip address 192.168.1.24 will be used throughout the remainder of this Readme but remember to replace it with your own ip*
+
+The first page a user reaches is the Select User page. 
+
+Click on your name to proceed to media voting.
+
+![screenshot](/screenshots/select_user.png)
+
+### Media Voting
+
+After the user has been selected, the media voting page will load. 
+ 
+This page shows the Title of the media, an image of the media (if available), and the 3 voting buttons
+
+![screenshot](/screenshots/vote_buttons.png)
+
+### Viewing voting status
+
+`http://192.168.1.24/list`
+
+Anyone can view a table of the current votes by going to this page 
+
+Above the table there is a simple bar chart showing how much of the library each user has voted so far.
+
+![screenshot](/screenshots/vote_results_summary.png)
+Here notice the last item in this example has received Delete votes from all users, so will be a candidate for actual deletion 
+
+### Deleting voted items from your library
+
+`http://192.168.1.24/ready_to_delete`
+
+Media items that everyone voted to delete are shown on a page along with a **Delete Forever** button.
+
+Pressing this will delete the media from Radarr (and the media file from your media storage).
+
+![screenshot](/screenshots/delete_after_vote_consensus.png)
+
+***
+
+## Future Improvements
+This is a quick project I threw together for my own use. I am not a professional developer and certainly not a front-end designer either, I am the ideas person.
+
+I may implement some of these improvements at some point however others are welcome to collaborate and contribute updates in the spirit of open source code.
+
+With that said here are some future improvements I can think of. These are listed in no particular order.
+
+* User Authentication - not a high priority for me for a LAN-only project such as this.
+* A way for a user to edit/change their vote
+* Periodically (maybe via a new url triggered from a cron job) remove all 'Keep' votes from the database, eg every 3 or 6 months. This would stop items ending up Keep forever and the resulting bloated media library. A better variation of this might be 'x months after that user last voted on that item' which would mean adding a vote timestamp to the voting db of course (not complex really).
+* A better nicer-looking GUI - no explanation needed, the current one is functional and 'not bad' but absolutely it is far from great!
+* A menu navbar for the GUI - another non-essential item hence not created initially, but would add a bit more glamour to this tool
+* Removal of orphan votes from the db (ie when the associated media no longer exists in Radarr). This could be done as part of the 'ready_to_delete' deletion function, however ideally would also detect and tidy if the media was deleted separately outside of Votearr.
+* Caching of the media posters - another one that doesn't seem that important to me for a low-usage system such as this, with 3 people voting you are downloading the image 3 times, big deal. However in the interests of optimum efficiency these could be saved and stored locally on the first vote, and then retrieved locally for the 2nd and 3rd person to vote on them. (I couldn't find a way to load the image directly from the Radarr Server without the end-user logging into Radarr before they vote in Votearr, which defeats the point of using the API)
+
+
+## 🙏 Acknowledgements
+
+| | |
+| :------------- |:-------------| 
+| [**Radarr**](https://radarr.video/)     | An excellent media manager for videos | 
+| [**arrapi**](https://github.com/Kometa-Team/ArrAPI) | Library of python bindings for Radarr and Sonarr |
+| [**Flask**](https://flask.palletsprojects.com/en/stable/) | Flask is a lightweight WSGI web application framework |
+| [**SQLite**](https://www.sqlite.org/) | A small, fast, self-contained, high-reliability, full-featured, SQL database engine |
+| [**Python**](https://www.python.org/) | A popular programming language suitable for a wide range of abilities and use cases|
+| [**Docker**](https://www.docker.com/) | An easy way to package code for use on different systems without dependency issues
+
+***
+
+## 💀 Privacy
+
+- The only data saved by votearr is saved into the .db file which is stored locally on your docker host machine (outside of the docker container)
+- This .db file contains votes, however does not show the media names or public references (such as imdb/tmdb ID's) - it only stores the Radarr media ID for it. These values are specific to your individual Radarr environment. In other words, if anyone obtains this .db file cannot determine what media you have in your library unless they also have access to *your* Radarr.
+
+***
---
+++ main.py
@@ -0,0 +1,263 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from arrapi import RadarrAPI, ArrException, NotFound
+import random
+import configparser
+import re
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/votes.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# Load settings.ini values
+config = configparser.ConfigParser()
+config.read('settings.ini')
+radarr_url = config['DEFAULT']['radarr_url']
+radarr_apikey = config['DEFAULT']['radarr_apikey']
+delete_votes_required = int(config['DEFAULT']['delete_votes_required'])
+voting_users = re.split(r',\s*', config['DEFAULT']['voting_users'])
+
+# Initialize Radarr API client
+radarr = RadarrAPI(radarr_url, radarr_apikey)
+
+# Define the Vote model
+class Vote(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    media_id = db.Column(db.Integer, nullable=False)
+    user = db.Column(db.String(80), nullable=False)
+    vote = db.Column(db.String(80), nullable=False)
+
+db.create_all()
+
+# Helper function to get a random media item that the user has not voted on
+def get_random_media(user):
+    # Get all media items from Radarr
+    media_items = radarr.all_movies()  # Returns a list of Movie objects
+
+    # Filter out media items that the user has already voted on or that do not have a file
+    voted_media_ids = [vote.media_id for vote in Vote.query.filter_by(user=user).all()]
+    unvoted_media_items = [
+        item for item in media_items if item.id not in voted_media_ids and item.hasFile
+    ]
+
+    if not unvoted_media_items:
+        # if there are no items which haven't been voted on, return the set of items which the user selected as Undecided, so they can reconsider these
+        decidelater_media_ids = [vote.media_id for vote in Vote.query.filter_by(user=user, vote='Decide later').all()]
+        decidelater_media_items = [
+        item for item in media_items if item.id in decidelater_media_ids and item.hasFile
+        ]
+        if not decidelater_media_items:
+            # There really is nothing left to vote on
+            return None
+        else:
+            unvoted_media_items = decidelater_media_items
+
+    # Choose a random unvoted media item
+    random_item = random.choice(unvoted_media_items)
+    print(f"random_item: {random_item}")
+    print(f"random_item.images: {random_item.images}")
+
+    # Extract the first image URL from the images list
+    poster_url = random_item.images[0].remoteUrl if random_item.images else None
+
+    return {
+        "id": random_item.id,
+        "title": random_item.title,
+        "poster_url": poster_url  # Use the first image URL as the poster
+    }
+
+
+def calc_user_voted_percent(user):
+    uservotes = Vote.query.filter_by(user=user).count()
+    
+    media_items = radarr.all_movies()  # Returns a list of Movie objects
+
+    # Filter out media items that the user has already voted on or that do not have a file
+    media_items_withfiles = [
+        item for item in media_items if item.hasFile
+    ]
+    count_media_items_withfiles = len(media_items_withfiles)
+    userpercentvoted = round(uservotes/count_media_items_withfiles*100)
+    if userpercentvoted > 100:
+        # fudge to prevent showing values over 100%, which happens if voted items have since been deleted from the library
+        userpercentvoted = 100
+    return userpercentvoted
+
+def calc_user_voted_percents(user):
+    # zero initial values so we don't return null
+    uservotes_keep = 0
+    uservotes_delete = 0
+    uservotes_decidelater = 0
+    
+    uservotes_keep = Vote.query.filter_by(user=user, vote='Keep').count()
+    uservotes_delete = Vote.query.filter_by(user=user, vote='Remove').count()
+    uservotes_decidelater = Vote.query.filter_by(user=user, vote='Decide later').count()
+    
+    media_items = radarr.all_movies()  # Returns a list of Movie objects
+
+    # Filter out media items that the user has already voted on or that do not have a file
+    media_items_withfiles = [
+        item for item in media_items if item.hasFile
+    ]
+    count_media_items_withfiles = len(media_items_withfiles)
+    if count_media_items_withfiles == 0:
+        # return safe values from this function if no media items found in Radarr
+        return 0,0,0
+    
+    userpercentvoted_keep = round(uservotes_keep/count_media_items_withfiles*100)
+    userpercentvoted_delete = round(uservotes_delete/count_media_items_withfiles*100)
+    userpercentvoted_decidelater = round(uservotes_decidelater/count_media_items_withfiles*100)
+    userpercent_unvoted = 100 - userpercentvoted_keep - userpercentvoted_delete - userpercentvoted_decidelater
+    
+    # fudges to prevent showing values over 100%, which happens if voted items have since been deleted from the library
+    if userpercentvoted_keep > 100:
+        userpercentvoted_keep = 100
+    
+    if userpercentvoted_delete > 100:
+        userpercentvoted_delete = 100
+        
+    if userpercentvoted_decidelater > 100:
+        userpercentvoted_decidelater = 100
+        
+    # return user percentage voted for keep, delete, decide later, and unvoted
+    return userpercentvoted_keep,userpercentvoted_delete,userpercentvoted_decidelater, userpercent_unvoted
+
+# Route to select a user
+@app.route('/')
+def index():
+    return render_template('index.html', users=voting_users)
+
+@app.route('/vote/<user>', methods=['GET', 'POST'])
+def vote(user):
+    if request.method == 'POST':
+        media_id = request.form['media_id']
+        vote_value = request.form['vote']
+
+        # Check if there is already a vote for this user and media item
+        existing_vote = Vote.query.filter_by(media_id=media_id, user=user).first()
+
+        if existing_vote:
+            # Update the existing vote
+            existing_vote.vote = vote_value
+        else:
+            # Create a new vote if no existing vote is found
+            new_vote = Vote(media_id=media_id, user=user, vote=vote_value)
+            db.session.add(new_vote)
+
+        # Commit the changes to the database
+        db.session.commit()
+
+        # Redirect to a new media item
+        return redirect(url_for('vote', user=user))
+
+    # Get a random media item for the user to vote on
+    media_item = get_random_media(user)
+    if not media_item:
+        return redirect(url_for('ready_to_delete'))
+
+    return render_template(
+        'vote.html',
+        user=user,
+        userpercentvoted = calc_user_voted_percent(user),
+        media_name=media_item["title"],
+        media_id=media_item["id"],
+        poster_url=media_item["poster_url"]
+    )
+
+@app.route('/list')
+def list_view():
+    # Aggregate votes by media and collect a list of unique users
+    media_votes = {}
+    user_list = set()
+
+    # Query all votes from the database
+    all_votes = Vote.query.all()
+
+    # Populate the media_votes and user_list
+    for vote in all_votes:
+        if vote.media_id not in media_votes:
+            media_votes[vote.media_id] = {user: None for user in user_list}
+        media_votes[vote.media_id][vote.user] = vote.vote
+        user_list.add(vote.user)
+
+    # Convert user_list to a sorted list
+    user_list = sorted(user_list)
+
+    userstats = []
+    for thisuser in user_list:
+        userpercentvoted_keep, userpercentvoted_delete, userpercentvoted_decidelater, userpercentvoted_unvoted = calc_user_voted_percents(thisuser)
+        userstats.append({"user":thisuser,"keep":userpercentvoted_keep,"delete":userpercentvoted_delete,"decidelater":userpercentvoted_decidelater,"unvoted":userpercentvoted_unvoted})
+        
+    # Fetch media details from Radarr for each media_id in media_votes
+    media_details = []
+    for media_id in media_votes.keys():
+        try:
+            media_item = radarr.get_movie(media_id)
+            media_details.append({
+                "id": media_id,
+                "title": media_item.title,
+                "year": media_item.year,
+                "votes": media_votes[media_id]
+            })
+        except Exception as e:
+            print(f"Error fetching media item {media_id}: {e}")
+
+    # sort alphabetically by title
+    media_details.sort(key=lambda x: x['title'])
+
+    # Pass the media_details and user_list to the template
+    return render_template('list.html', media_list=media_details, user_list=user_list, userstats=userstats)
+
+@app.route('/delete/media_id/<media_id>')
+def delete_now(media_id):
+    media_id = int(media_id)
+    
+    # safety check if we have 'delete_votes_required' delete votes for this movie id
+    this_media_votes = Vote.query.filter_by(media_id=media_id, vote='Remove').count()
+    if this_media_votes >= delete_votes_required:    
+        # call arrapi to delete this movie id
+        try:
+            output = radarr.delete_movie(media_id)
+        except (ArrException, NotFound):
+            movie_deleted = "Error movie not found"
+            return render_template('delete_status.html', movie_deleted=movie_deleted)
+            
+        # return: display outcome success/fail, including the deleted movie_id.name
+        if output:
+            movie_deleted = f"{output.title} deleted successfully"
+        else:
+            movie_deleted = "Error deleting movie"
+    else:
+        this_movie = radarr.get_movie(media_id)
+        movie_deleted = f"Unable to delete {this_movie.title}, need {delete_votes_required} votes only have {this_media_votes}" 
+    return render_template('delete_status.html', movie_deleted=movie_deleted)        
+        
+# Route to view media items that are ready to be requested (manually) for actual library removal
+@app.route('/ready_to_delete')
+def ready_to_delete():
+    # Find all media items with votes
+    media_votes = {}
+    for vote in Vote.query.all():
+        if vote.media_id not in media_votes:
+            media_votes[vote.media_id] = {"keep": 0, "remove": 0, "decide later": 0}
+        media_votes[vote.media_id][vote.vote.lower()] += 1
+
+    # Find which of these media items has met the 'delete_votes_required' for "remove" votes
+    to_delete = []
+    for media_id, votes in media_votes.items():
+        if votes["remove"] >= delete_votes_required:
+            try:
+                media_item = radarr.get_movie(media_id)
+                to_delete.append({
+                    "title": media_item.title,
+                    "year": media_item.year,
+                    "delete_url": "/delete/media_id/"+str(media_id)
+                })
+            except (ArrException, NotFound):
+                # movie not found - means it has already been deleted, no need to panic!
+                continue
+    return render_template('ready_to_delete.html', to_delete=to_delete)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
